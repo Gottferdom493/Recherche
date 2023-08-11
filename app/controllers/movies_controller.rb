59e62e174cd6530f4a1380317d828cc14e538513
raphael <raphael.bichet@yahoo.com>
2023-08-11 11:30:39 +0200

    afinement de la recherche apres avoir réussie la recherche juste dans les titres, on rajoute la recherche titre plus synopsie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,8 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     if params[:query].present?
-      @movies = @movies.where(title: params[:query])
+      sql_subquery = "title ILIKE :query OR synopsis ILIKE :query"
+      @movies = @movies.where(sql_subquery, query: "%#{params[:query]}%")
     end
   end
 
