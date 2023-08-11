e08e49d2452aa18818c5ba53b9dcd9e41a7874e4
raphael <raphael.bichet@yahoo.com>
2023-08-11 11:56:30 +0200

    ajout des 5 facons de paramétrer une barre de rechercher à l'aide rails

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,19 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
 
-    # 3/ ---------- Recherche dans le titre et le synopsie + dans la table reference ----------
+    # 4/ ---------- Permet la recherche entre plusieures mots entier seulelment dans les deux tables ----------
+    # if params[:query].present?
+    #   sql_subquery = <<~SQL
+    #     movies.title @@ :query
+    #     OR movies.synopsis @@ :query
+    #     OR directors.first_name @@ :query
+    #     OR directors.last_name @@ :query
+    #   SQL
+    #   @movies = @movies.joins(:director).where(sql_subquery, query: params[:query])
+    # end
+    # 4/ ---------- -------------------------------- ----------
+
+    # 3/ ------ Recherche dans la table Movies le titre et le synopsie + dans la table reference Directors les noms ------
     if params[:query].present?
       sql_subquery = <<~SQL
         movies.title ILIKE :query
@@ -15,18 +27,24 @@ class MoviesController < ApplicationController
     end
     # 3/ ---------- -------------------------------- ----------
 
-    # 2/ ---------- Recherche dans le titre et le synopsie ----------
+    # 2/ ---------- Recherche dans la table Movies le titre et le synopsie ----------
     # if params[:query].present?
     #   sql_subquery = "title ILIKE :query OR synopsis ILIKE :query"
     #   @movies = @movies.where(sql_subquery, query: "%#{params[:query]}%")
     # end
     # 2/ ---------- -------------------------------- ----------
 
-    # 1/ ---------- Recherche dans le titre seulment ----------
+    # 1/ ---------- Recherche dans la table Movies le titre simplifié ----------
     # if params[:query].present?
     #   @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%") permet de
     # end
     # 1/ ---------- -------------------------------- ----------
+
+    # 0/ ---------- Recherche dans la table Movies le titre entier seulment ----------
+    # if params[:query].present?
+    #   @movies = @movies.where(title: params[:query])
+    # end
+    # 0/ ---------- -------------------------------- ----------
 
   end
 
